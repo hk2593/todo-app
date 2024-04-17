c6827e32e30e08b07b57c26e38c4f80d469bc5d7
Harsh <sunilkumar47260@gmail.com>
2024-04-17 20:57:43 +0530

    intial files

---
+++ src/store/authslice.js
@@ -0,0 +1,39 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  token: null,
+  name: null,
+  todos:[],
+  API_KEY:'http://localhost:4000/',
+  addtodo:0,
+  date:null
+  // Add follow status field
+};
+
+export const userSlice = createSlice({
+  name: 'auth',
+  initialState,
+ 
+  reducers: {
+    setLogin: (state, action) => {
+      state.name = action.payload.name;
+      state.token = action.payload.token;
+    },
+    setLogout: (state) => {
+      state.name = null;
+      state.token = null; 
+    },
+    setaddtodo: (state)=>{
+      state.addtodo=1-state.addtodo;
+    },
+    setTodos: (state,action)=>{
+      state.todos=action.payload;
+    },
+    setDate:(state,action)=>{
+      state.date=action.payload;
+    }
+  },
+});
+
+export const { setLogin, setLogout, setTodos,setaddtodo,setDate} = userSlice.actions;
+export default userSlice.reducer;
